edd04089bc08a9f83ec5fa19c7fb7b094a95995c
Guilherme De Castro Amaro <gude-cas@c1r15s13.42lisboa.com>
2024-06-15 19:09:07 +0100

    murder

--- cpp05/ex01/main.cpp
+++ cpp05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gude-cas <gude-cas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/15 15:36:03 by gude-cas          #+#    #+#             */
-/*   Updated: 2024/06/15 19:06:30 by gude-cas         ###   ########.fr       */
+/*   Updated: 2024/06/15 19:07:59 by gude-cas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,8 +32,8 @@ int main(void)
 		std::cout << "Bureaucrat " << bro.getName() << " grade: "<< bro.getGrade() << std::endl;
 		bro.incrementGrade(4);
 		std::cout << "Bureaucrat " << bro.getName() << " grade: "<< bro.getGrade() << std::endl;
-        // bro.decrementGrade(8);
-		// std::cout << "Bureaucrat " << bro.getName() << " grade: "<< bro.getGrade() << std::endl;
+        bro.decrementGrade(8);
+		std::cout << "Bureaucrat " << bro.getName() << " grade: "<< bro.getGrade() << std::endl;
 
 		Form form1("form1", 3, 3);
 		Form form2("form2", 40, 3);
